6191af45c177b3ef0ca43a99ed4511064d57efb2
mohammedsalmannn <rmdsalman001@gmail.com>
2021-05-10 09:46:58 +0530

    Added : muliple_loops

--- FilpcoinSimulator.sh
+++ FilpcoinSimulator.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash 
 
 echo "*** Welcome To Flip Coin Simulation Problem ***"
 
@@ -11,3 +11,21 @@ then
 else
         echo "Tail Is Winner"
 fi
+echo "To Loop Through Multile times To Flip A Coin To Display No. Of Times Head And Tail Has Won"
+
+headCount=0;
+tailCOunt=0;
+num=42;
+for((index=1; index<=$num; index++))
+do
+	if [[ $((RANDOM%2)) -eq 1 ]]
+	then
+		echo "Head is the winner"
+		((headCount++))
+
+	else
+		echo "Tail is the winner"
+		((tailCount++))
+
+	fi
+done
